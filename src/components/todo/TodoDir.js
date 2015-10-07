6b67adcdbe2ff44334ec2d3daab85d1b906a394a
Daniel Prado <dprado@underarmour.com>
2015-10-07 02:11:17 -0600

    First steps.

--- src/components/todo/TodoDir.js
+++
@@ -1,8 +0,0 @@
-module.exports = function TodoDir() {
-    return {
-        restrict: 'AE',
-        template: require('./todo.html'),
-        controller: 'TodoCtrl as todo',
-        scope: {}
-    };
-};
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
 var webpack = require('webpack'),
-    HtmlWebpackPlugin = require('html-webpack-plugin'),
-    IS_BUILD = process.env.IS_BUILD === 'true';
+    HtmlWebpackPlugin = require('html-webpack-plugin');
 
 exports.context = __dirname + '/src';
 
-if(!IS_BUILD) {
-    exports.contentBase = "http://localhost:3000/";
-}
-
 exports.entry = {
-    'app': IS_BUILD? './index.js' : ['webpack/hot/dev-server', './index.js']
+    'app': './index.js'
 };
 
 exports.output = {
@@ -17,14 +12,7 @@ exports.output = {
     filename: '[name].min.js'
 };
 
-if(!IS_BUILD) {
-    exports.output.publicPath = 'http://localhost:9080/assets/';
-}
-
 exports.resolve = {
-    alias: {
-        components: '../components'
-    },
     extensions: [
         '', '.js', '.common.js'
     ]
